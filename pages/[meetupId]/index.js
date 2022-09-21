1112ddd2d8e4178b8717873d82a1df70feeaa73c
Ryan Cabanas <ryangithub777@gmail.com>
2022-09-20 22:20:56 -0400

    End of Lesson #334/335
    - Update the `index.js` page and set it up to expect to work with a
      `MeetupDetail` component.  Pass some anticipated props.  Pass some
      dummy data for now.
    
    - Create the `MeetupDetail` component and a module CSS file to go with
      it.  Add a little bit of CSS.

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
+      title="A 1st Meetup"
+      address="123 Main St, City, State"
+      description="This is a 1st meetup."
+    />
+  );
+};
+
+export default MeetupDetails;
